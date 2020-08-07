a60b5d7d75f80ab9e49aa17294a51e7505a5dae9
attiqa304 <fa19-rcs-013@student.comsats.edu.pk>
2020-08-07 05:55:35 +0500

    done

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class menu : MonoBehaviour
+{
+    public void PalypenguinsGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PlayHummingBirdGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void PalindromeWorld()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void MatchingParenthesisWorld()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+    public void Quit()
+    {
+        #if UNITY_EDITOR                UnityEditor.EditorApplication.isPlaying = false;        #else                Application.Quit();        #endif        //UnityEditor.EditorApplication.isPlaying = false;    }
+
+    
+    public void OpenLink()
+    {
+        
+        Application.OpenURL("http://www.niazilab.com");
+    }
+}
+
+
